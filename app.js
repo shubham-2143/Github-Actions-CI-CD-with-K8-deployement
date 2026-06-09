29f1eb577e85ccfb5c53a91fc3aff58938dd27cb
shubham-2143 <shubhamsontakke0062@gmail.com>
2026-06-09 16:12:44 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
+const express = require('express');
+const app = express();
 
+app.get('/', (req, res) => {
+  res.send('Hello from Kubernetes CI/CD Project');
+});
+
+app.listen(3000, () => {
+  console.log('App running on port 3000');
+});
