877fe1554577dcf72aec43b8644f388b0bdb6695
shubham-2143 <shubhamsontakke0062@gmail.com>
2026-06-09 18:34:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('Hello from Kubernetes CI/CD Project');
+  res.send('Hello from Kubernetes Github Project');
 });
 
 app.listen(3000, () => {
